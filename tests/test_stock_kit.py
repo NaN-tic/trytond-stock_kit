f50b29d1e61fe4c3f3bd0dc45d50567ed6b4557f
resteve <resteve@zikzakmedia.com>
2015-12-01 11:24:18 +0100

    Tests

--- tests/test_stock_kit.py
+++ tests/test_stock_kit.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part stock_kit module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the stock_kit module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockKitTestCase(unittest.TestCase):
+class StockKitTestCase(ModuleTestCase):
     'Test Stock Kit module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_kit')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_kit')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_kit'
 
 
 def suite():
