56cac7aa2ec5caab8893cb02fb8ea0079591865e
C?dric Krier <ced@b2ck.com>
2018-01-24 15:30:31 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
-#This file is part stock_kit module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part stock_kit module for Tryton.  The COPYRIGHT file at the top
+# level of this repository contains the full copyright notices and license
+# terms.
 from trytond.pool import Pool
-from .product import *
-from .inventory import *
+from . import product
+from . import inventory
 
 
 def register():
     Pool.register(
-        Template,
-        Product,
-        InventoryLine,
+        product.Template,
+        product.Product,
+        inventory.InventoryLine,
         module='stock_kit', type_='model')
