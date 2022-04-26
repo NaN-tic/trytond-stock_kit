6666f17ce4271aeeb775a9518071104674d60f4a
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:24:33 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part stock_kit module for Tryton.  The COPYRIGHT file at the top
-# level of this repository contains the full copyright notices and license
-# terms.
-try:
-    from trytond.modules.stock_kit.tests.test_stock_kit import suite
-except ImportError:
-    from .test_stock_kit import suite
-
-__all__ = ['suite']
